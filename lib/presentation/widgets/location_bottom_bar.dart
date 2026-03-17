5c68178d32f543290da857aa84e016d4c32cd06d
daisukeucc <daisukeucc@gmail.com>
2026-03-17 11:23:01 +0900

    Revert "コメント削除"
    This reverts commit 2b92280c26182689707dafb787410004cba3296d.

--- lib/presentation/widgets/location_bottom_bar.dart
+++ lib/presentation/widgets/location_bottom_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// 画面下部の再生/停止バーと位置取得中のプログレスバー
 class LocationBottomBar extends StatelessWidget {
   const LocationBottomBar({
     super.key,
@@ -12,6 +13,8 @@ class LocationBottomBar extends StatelessWidget {
   final bool isStreamActive;
   final VoidCallback onTap;
   final ValueNotifier<double>? progressBarValue;
+
+  /// true のときボタンをグレー表示する（LOWモード時）
   final bool isLowMode;
 
   @override
